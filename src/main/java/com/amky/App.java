aa503fb2b8f8569655a337f33b47db1312509987
Makarand Kulkarni <makarchus@yahoo.com>
2020-01-26 08:10:29 -0500

    Basic SpriingBootREST Template

--- src/main/java/com/amky/App.java
+++ src/main/java/com/amky/App.java
@@ -12,8 +12,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 
 @SpringBootApplication
-public class EmployeeREST extends SpringBootServletInitializer  {
+public class App extends SpringBootServletInitializer  {
 	public static void main(String[] args) {
-        SpringApplication.run(EmployeeREST.class, args);
+        SpringApplication.run(App.class, args);
     }
 }
